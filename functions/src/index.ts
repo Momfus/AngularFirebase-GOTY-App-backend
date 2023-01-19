bd6bb086e0425ef27bb60dd279dc76e77878c6bf
Momfus <momfus@outlook.com>
2023-01-19 15:26:49 -0300

    Get the game goty registers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,11 +9,25 @@ admin.initializeApp({
     databaseURL: "https://firestore-graph-goty-default-rtdb.firebaseio.com"
 });
 
+const db = admin.firestore();
+
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
-//
+
+// # CLOUD FUNCTIONS #
+
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.json({
    message: "Hello world from firebase!!"
   });
+});
+
+export const getGoty = functions.https.onRequest( async(request, response) => {
+
+  const gotyRef = db.collection('goty');
+  const docsSnap = await gotyRef.get();
+  const games = docsSnap.docs.map( doc => doc.data() ); // Recover the information of each register
+
+  response.json( games ); 
+
 });
