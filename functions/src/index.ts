5a8a19f16d8382e30c69fe19797cd48d55fed634
Momfus <momfus@outlook.com>
2023-01-19 15:05:08 -0300

    add base credential setup (ignore the credential file to use)

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,19 @@
+
 import * as functions from "firebase-functions";
+import * as admin from 'firebase-admin';
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://firestore-graph-goty-default-rtdb.firebaseio.com"
+});
 
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.json({
+   message: "Hello world from firebase!!"
+  });
+});
